e9dbe9fd284c1a424940e563b0a19ebf9197b7e2
Justin Maple <justinamaple@gmail.com>
2019-08-18 17:22:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,3 +9,22 @@ end
 def using_pop(array)
   array.pop()
 end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array, element)
+  array.concat(element)
+end
+
+def using_insert(array, element)
+  array.insert()
